62a0191911b2336c9fe2a59d1f38fe8349746f33
Alex Radulescu <radulescu.alexandru.doru@gmail.com>
2024-03-12 22:34:58 +0800

    Added Basic ParkUI

---
+++ app/components/ui/icon.tsx
@@ -0,0 +1,15 @@
+import { forwardRef, type ReactElement } from 'react'
+import { ark } from '@ark-ui/react/factory'
+
+import { styled, type HTMLStyledProps } from '../../../styled-system/jsx'
+import { icon, type IconVariantProps } from '../../../styled-system/recipes'
+
+export interface IconProps extends IconVariantProps, HTMLStyledProps<'svg'> {
+  children: ReactElement
+}
+
+export const Icon = forwardRef<SVGSVGElement, IconProps>((props, ref) => {
+  return <StyledIcon ref={ref} asChild {...props} />
+})
+
+const StyledIcon = styled(ark.svg, icon)
